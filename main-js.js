21213a87758038d8dfbe53975f333d5e2010ea12
Pavel Dubina <Pavel_Dubina@epam.com>
2020-10-28 17:31:03 +0300

    feat: new func

--- main-js.js
+++ main-js.js
@@ -1,11 +1,19 @@
 const fullSize = document.querySelector('.full--button');               // кнопка увеличения размера экрана
 const container = document.querySelector('.container');                 // главная страница
 
-fullSize.addEventListener('click', () => {                              // при нажатии на кнопку увеличения размера экрана меняем значение в localStorage в зависимости от наличия класса у главной страницы и в последующем используем это в скрипте игры 
-    container.classList.toggle('full-screen');
+
+function localValue(){
     if(container.classList.contains('full-screen')){
         localStorage.setItem('full', true)
     } else {
         localStorage.setItem('full', false)
     }
+}
+
+fullSize.addEventListener('click', () => {                              // при нажатии на кнопку увеличения размера экрана меняем значение в localStorage в зависимости от наличия класса у главной страницы и в последующем используем это в скрипте игры 
+    container.classList.toggle('full-screen');
+    localValue();
+})
+window.addEventListener('load', ()=> {
+    localValue();
 })
