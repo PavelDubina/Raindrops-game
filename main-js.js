94f9f6a2dc8b56bbf639bf82da3e63ce297685d6
Pavel Dubina <Pavel_Dubina@epam.com>
2020-10-28 17:09:22 +0300

    feat: add comments

--- main-js.js
+++ main-js.js
@@ -1,7 +1,7 @@
-const fullSize = document.querySelector('.full--button');
-const container = document.querySelector('.container');
+const fullSize = document.querySelector('.full--button');               // кнопка увеличения размера экрана
+const container = document.querySelector('.container');                 // главная страница
 
-fullSize.addEventListener('click', () => {
+fullSize.addEventListener('click', () => {                              // при нажатии на кнопку увеличения размера экрана меняем значение в localStorage в зависимости от наличия класса у главной страницы и в последующем используем это в скрипте игры 
     container.classList.toggle('full-screen');
     if(container.classList.contains('full-screen')){
         localStorage.setItem('full', true)
